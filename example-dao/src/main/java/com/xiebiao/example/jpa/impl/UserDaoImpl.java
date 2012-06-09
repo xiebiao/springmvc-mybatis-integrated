1ca377405af485a32b1845090e56f36a5d4ea421
xiaog <joyrap@gmail.com>
2012-06-09 11:04:25 +0800

    添加spring jpa依赖

---
+++ example-dao/src/main/java/com/xiebiao/example/jpa/impl/UserDaoImpl.java
@@ -0,0 +1,9 @@
+package com.xiebiao.example.jpa.impl;
+
+import org.springframework.orm.jpa.support.JpaDaoSupport;
+
+import com.xiebiao.example.dao.UserDao;
+
+public class UserDaoImpl extends JpaDaoSupport implements UserDao {
+
+}
