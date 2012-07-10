7e79c244a83ea36f1e763abca4eaccf62a330a67
xiaog <joyrap@gmail.com>
2012-07-10 18:36:10 +0800

    测试tomcat cometd

---
+++ example-web/src/main/java/com/xiebiao/example/controller/TomcatWeatherServlet.java
@@ -0,0 +1,14 @@
+package com.xiebiao.example.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+
+public class TomcatWeatherServlet extends HttpServlet implements CometProcessor {
+	// http://www.ibm.com/developerworks/web/library/wa-cometjava/
+
+}
