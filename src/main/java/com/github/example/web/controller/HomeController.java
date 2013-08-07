daffa99b4f285a3db049d3a60439e9df26eebed5
xiebiao <joyrap@gmail.com>
2013-08-07 15:01:25 +0800

    更新Velocity配置

---
+++ src/main/java/com/github/example/web/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.github.example.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public ModelAndView user() {
+        System.out.println(HomeController.class.getName());
+        ModelAndView mv = new ModelAndView();
+        return mv;
+    }
+}
