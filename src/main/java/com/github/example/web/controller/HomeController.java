80e27ad95a3ae17dde22bee8984dda910fa16caf
Barry Xie <joyrap@gmail.com>
2014-11-19 23:35:14 +0800

    Add hibernate-validator for validation

--- src/main/java/com/github/example/web/controller/HomeController.java
+++ src/main/java/com/github/example/web/controller/HomeController.java
@@ -1,37 +1,44 @@
 package com.github.example.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 @Controller
 public class HomeController {
 
-  @RequestMapping(value = "/", method = RequestMethod.GET)
-  public ModelAndView index(HttpServletRequest request) {
-    WebApplicationContext applicationContext =
-        WebApplicationContextUtils.getWebApplicationContext(request.getServletContext());
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView index(HttpServletRequest request) {
+        WebApplicationContext applicationContext =
+            WebApplicationContextUtils.getWebApplicationContext(request.getServletContext());
 
-    // Map<String, HandlerMapping> matchingBeans =
-    // BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, HandlerMapping.class,
-    // true, false);
-    // for(HandlerMapping handlerMapping:matchingBeans.values()){
-    // System.out.println(handlerMapping.getClass());
-    // }
+        // Map<String, HandlerMapping> matchingBeans =
+        // BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, HandlerMapping.class,
+        // true, false);
+        // for(HandlerMapping handlerMapping:matchingBeans.values()){
+        // System.out.println(handlerMapping.getClass());
+        // }
 
-    ModelAndView mv = new ModelAndView("index");
-    return mv;
-  }
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
 
-  @RequestMapping(value = "/home", method = RequestMethod.GET)
-  public ModelAndView user() {
-    System.out.println(HomeController.class.getName());
-    ModelAndView mv = new ModelAndView();
-    return mv;
-  }
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public ModelAndView user(@Valid UserForm userForm,
+        BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            System.out.println("" + bindingResult.getAllErrors().get(0).getDefaultMessage());
+
+        }
+        System.out.println(HomeController.class.getName());
+        ModelAndView mv = new ModelAndView();
+        return mv;
+    }
 }
