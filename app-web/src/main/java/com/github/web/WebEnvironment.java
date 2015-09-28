cc63c424f758e916c54b001a04f436d63ce22383
xiebiao <xiebiao@jd.com>
2015-09-28 15:47:26 +0800

    add environment util

---
+++ app-web/src/main/java/com/github/web/WebEnvironment.java
@@ -0,0 +1,25 @@
+package com.github.web;
+
+/**
+ * Created by xiebiao on 2015/9/28.
+ */
+public class WebEnvironment {
+    private String domain;
+    private String home;
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public void setHome(String home) {
+        this.home = home;
+    }
+}
