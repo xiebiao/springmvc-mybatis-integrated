b9a205a2289a226388c52e94a1439a5e46829a49
bjxieb <xiebiao@jd.com>
2016-10-24 09:52:36 +0800

    test jqeury ajax post array

--- app-web/src/main/java/com/github/example/web/controller/HomeController.java
+++ app-web/src/main/java/com/github/example/web/controller/HomeController.java
@@ -1,9 +1,6 @@
 package com.github.example.web.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.validation.Valid;
+import com.github.web.Result;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -12,31 +9,47 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 @Controller
 public class HomeController {
 
-  @RequestMapping(value = "/index", method = RequestMethod.GET)
-  public ModelAndView index() {
-    ModelAndView mv = new ModelAndView("index");
-    return mv;
-  }
-
-  @RequestMapping(value = "/home", method = RequestMethod.GET)
-  public ModelAndView user(@Valid UserForm userForm, BindingResult bindingResult) {
-    if (bindingResult.hasErrors()) {
-      System.out.println("" + bindingResult.getAllErrors().get(0).getDefaultMessage());
-
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
     }
-    System.out.println(HomeController.class.getName());
-    ModelAndView mv = new ModelAndView();
-    return mv;
-  }
 
-  @ResponseBody
-  @RequestMapping(value = "/messages.json", method = RequestMethod.GET)
-  public Map<String, String> messages() {
-    Map<String, String> map = new HashMap<>();
-    map.put("name", "xiebiao");
-    return map;
-  }
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public ModelAndView user(@Valid UserForm userForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            System.out.println("" + bindingResult.getAllErrors().get(0).getDefaultMessage());
+
+        }
+        System.out.println(HomeController.class.getName());
+        ModelAndView mv = new ModelAndView();
+        return mv;
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @ResponseBody
+    public Result save(@Valid UserForm userForm, BindingResult bindingResult) {
+        Result result = new Result();
+        return result;
+    }
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public ModelAndView add(@Valid UserForm userForm, BindingResult bindingResult) {
+        ModelAndView mv = new ModelAndView("test/add");
+        return mv;
+    }
+    @ResponseBody
+    @RequestMapping(value = "/messages.json", method = RequestMethod.GET)
+    public Map<String, String> messages() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "xiebiao");
+        return map;
+    }
 }
